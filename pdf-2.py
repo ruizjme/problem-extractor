cdfa58b0ae37cd0047155b08518b79a082b15e6e
ruizjme <hello@ruizjaime.com>
2016-10-02 22:31:32 +1100

    Working on extracting text.

--- pdf-2.py
+++ pdf-2.py
@@ -2,9 +2,9 @@ from PyPDF2 import PdfFileReader
 
 b = 'book-long.pdf'
 
-print("READING: ["+b+"]")
+print('READING: ['+b+']')
 
-i = int(input("Insert page number: "))-1
+i = int(input('Insert page number: ')) #this here stores the page number
 
 f = open(b,'rb')
 
@@ -14,30 +14,12 @@ n = 1
 m = 0
 
 try:
-    contents = PdfFileReader(f).getPage(i).extractText()
+    contents = PdfFileReader(f).getPage(i-1).extractText()
     for l in contents:
-        if l == str(n):
-            #if l+1 == ".":
-            print(n)
-            n = n+1
-        elif n >= 10:
-            if l == str(m):
-                print(n)
-                n = n+1
-                m = m+1
-            
 
-        o = open('output.html','w')
+        o = open(str(i)+'.txt','w')
         o.write(contents.encode('utf-8')) #str(r).encode('utf-8')
         o.close()
 finally:
         f.close()
-        print("OPERATION COMPLETE.")
-        while True:
-                try:
-                        ri = int(raw_input("Index? - "))
-                        print("Index number "+str(ri)+" corresponds to \""+str(r[ri])+"\"")
-                except ValueError:
-                        print("Invalid input.")
-                except IndexError:
-                        print("Out of scope.")
+        print('COMPLETE: ['+str(i)+'.txt]')
