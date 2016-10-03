2a18eb3e4941ba56ba34736048628b89f1b2e206
ruizjme <hello@ruizjaime.com>
2016-10-03 14:47:36 +1100

    Use of regex expressions to find question numbers and match them

--- pdf-2.py
+++ pdf-2.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfFileReader
+import re
 
 b = 'book-long.pdf'
 
@@ -8,18 +9,14 @@ i = int(input('Insert page number: ')) #this here stores the page number
 
 f = open(b,'rb')
 
-r = []
-
-n = 1
-m = 0
-
 try:
     contents = PdfFileReader(f).getPage(i-1).extractText()
-    for l in contents:
 
-        o = open(str(i)+'.txt','w')
-        o.write(contents.encode('utf-8')) #str(r).encode('utf-8')
-        o.close()
+    o = open(str(i)+'.txt','w')
+    o.write(contents.encode('utf-8'))
+    o.close()
 finally:
-        f.close()
-        print('COMPLETE: ['+str(i)+'.txt]')
+    f.close()
+    print('COMPLETE: ['+str(i)+'.txt]')
+    matches = re.findall("([0-9]+\. )", contents)
+    print(matches)
